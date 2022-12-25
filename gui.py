73e79d24ab7e33d51f56068a1e3839c3dea55a08
sparkvilla <diego.villamaina@gmail.com>
2022-12-25 20:52:22 +0100

    remove old code

--- gui.py
+++ gui.py
@@ -17,7 +17,6 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
 
-<<<<<<< HEAD
 chess = Chess("./test_game_005.json5")
 positions = None
 avail = None
@@ -26,16 +25,6 @@ edibl = None
 game_exit = False
 while not game_exit:
     chess.is_check()
-=======
-board = Board()
-chess = Chess(board)
-avail = None
-edibl = None
-restricted = None
-
-game_exit = False
-while not game_exit:
->>>>>>> ba1fbbd4e96c6175db2fc62424df18ed5aec1a32
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
@@ -47,19 +36,10 @@ while not game_exit:
                 chess.set_piece_from_coords(x_start, y_start)
                 print(f"object: {chess.piece}")
                 print(f"player: {chess.player}")
-
-<<<<<<< HEAD
-=======
-                if restricted:
-                    if chess.piece not in restricted:
-                        continue
-
->>>>>>> ba1fbbd4e96c6175db2fc62424df18ed5aec1a32
                 if chess.piece == 0 or chess.piece.color != chess.player:
                     print("not allowed")
                     continue
 
-<<<<<<< HEAD
                 positions = chess.get_positions()
 
                 if chess.is_restricted():
@@ -74,14 +54,6 @@ while not game_exit:
 
                 avail, edibl = chess.get_avail_and_edibl(positions)
 
-=======
-                if chess.is_pinned():
-                    print("pinned")
-                    continue
-
-                positions = chess.get_positions()
-                avail, edibl = chess.get_avail_and_edibl(positions)
->>>>>>> ba1fbbd4e96c6175db2fc62424df18ed5aec1a32
                 print(f"avail: {avail}")
                 print(f"edibl: {edibl}")
         if event.type == pygame.MOUSEBUTTONUP:
@@ -92,25 +64,13 @@ while not game_exit:
                 if chess.piece == 0 or chess.piece.color != chess.player:
                     print("not allowed")
                     continue
-<<<<<<< HEAD
                 an_end = chess.board.get_an_from_mouse(x_end, y_end)
                 chess.move_piece(positions, an_end)
-=======
-                an_end = board.get_an_from_mouse(x_end, y_end)
-                chess.move_piece(positions, an_end)
-                if chess.is_check():
-                    print("check")
-                    restricted = chess.get_uncheck()
->>>>>>> ba1fbbd4e96c6175db2fc62424df18ed5aec1a32
 
                 avail = None
                 edibl = None
 
     screen.fill(COLOR_SCREEN)
-<<<<<<< HEAD
     chess.board.draw_board(screen, avail, edibl)
-=======
-    board.draw_board(screen, avail, edibl)
->>>>>>> ba1fbbd4e96c6175db2fc62424df18ed5aec1a32
     pygame.display.flip()
     clock.tick(30)
